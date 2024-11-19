bae09e13ed24ce600df3bd682e1ce2097b20dcec
wenxxxxfu <fwen1999@126.com>
2024-11-19 20:14:46 +0800

    sync_fifo_ptr

--- fifo/sync_fifo_ptr/sync_fifo_ptr.sv
+++ fifo/sync_fifo_ptr/sync_fifo_ptr.sv
@@ -7,14 +7,14 @@ module	sync_fifo_ptr
     parameter   DATA_DEPTH = 'd16 							//FIFO深度
 )
 (
-    input                                   clk		,       //系统时钟
-    input                                   rst_n	,       //低电平有效的复位信号
-    input   [DATA_WIDTH-1:0]                data_in	,       //写入的数据
-    input                                   rd_en	,       //读使能信号，高电平有效
-    input                                   wr_en	,       //写使能信号，高电平有效
+    input                                   clk		    ,       //系统时钟
+    input                                   rst_n	    ,       //低电平有效的复位信号
+    input   [DATA_WIDTH-1:0]                data_in	    ,       //写入的数据
+    input                                   rd_en	    ,       //读使能信号，高电平有效
+    input                                   wr_en	    ,       //写使能信号，高电平有效
 															
-    output  reg [DATA_WIDTH-1:0]            data_out,       //输出的数据
-    output                                  empty	,       //空标志，高电平表示当前FIFO已被写满
+    output  reg [DATA_WIDTH-1:0]            data_out    ,       //输出的数据
+    output                                  empty	    ,       //空标志，高电平表示当前FIFO已被写满
     output                                  full            //满标志，高电平表示当前FIFO已被读空
  
 
