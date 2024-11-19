3e57c03a664b8e5e08e07c16df363bfdd9df7a15
wenxxxxfu <fwen1999@126.com>
2024-11-19 20:08:59 +0800

    sync_fifo_ptr

--- fifo/sync_fifo_counter/sync_fifo_cnt.sv
+++ fifo/sync_fifo_counter/sync_fifo_cnt.sv
@@ -3,18 +3,18 @@
 //计数器法实现同步FIFO
 module	sync_fifo_cnt
 #(
-    parameter   DATA_WIDTH = 'd8  ,							//FIFO位宽
-    parameter   DATA_DEPTH = 'd16 							//FIFO深度
+    parameter   DATA_WIDTH = 'd8                    ,       //FIFO位宽
+    parameter   DATA_DEPTH = 'd16                           //FIFO深度
 )
 (
-    input                                   clk		,		//系统时钟
+    input                                   clk		,       //系统时钟
     input                                   rst_n	,       //低电平有效的复位信号
     input   [DATA_WIDTH-1:0]                data_in	,       //写入的数据
     input                                   rd_en	,       //读使能信号，高电平有效
     input                                   wr_en	,       //写使能信号，高电平有效
 															
-    output  reg [DATA_WIDTH-1:0]            data_out,	    //输出的数据
-    output                                  empty	,	    //空标志，高电平表示当前FIFO已被写满
+    output  reg [DATA_WIDTH-1:0]            data_out,       //输出的数据
+    output                                  empty	,       //空标志，高电平表示当前FIFO已被写满
     output                                  full	,       //满标志，高电平表示当前FIFO已被读空
     output  reg [$clog2(DATA_DEPTH) : 0]    fifo_cnt		//$clog2是以2为底取对数	
 
--- fifo/sync_fifo_counter/tb_sync_fifo_cnt.sv
+++ fifo/sync_fifo_counter/tb_sync_fifo_cnt.sv
@@ -6,16 +6,16 @@ module tb_sync_fifo_cnt();
     parameter   DATA_WIDTH = 8  ;			//FIFO位宽
     parameter   DATA_DEPTH = 8 ;			//FIFO深度
     
-    reg									clk		;
-    reg									rst_n	;
-    reg		[DATA_WIDTH-1:0]			data_in	;
-    reg									rd_en	;
-    reg									wr_en	;
+    reg									clk     ;
+    reg									rst_n   ;
+    reg		[DATA_WIDTH-1:0]			data_in ;
+    reg									rd_en   ;
+    reg									wr_en   ;
                             
     wire	[DATA_WIDTH-1:0]			data_out;	
-    wire								empty	;	
-    wire								full	;
-    wire	[$clog2(DATA_DEPTH) : 0]	fifo_cnt;
+    wire								empty   ;	
+    wire								full    ;
+    wire	[$clog2(DATA_DEPTH) : 0]    fifo_cnt;
     
     
     //------------<例化被测试模块>----------------------------------------
@@ -39,8 +39,8 @@ module tb_sync_fifo_cnt();
  
     //------------<设置初始测试条件>----------------------------------------
     initial begin
-	clk = 1'b0;							//初始时钟为0
-	rst_n <= 1'b0;						//初始复位
+	clk = 1'b0;	                        //初始时钟为0
+	rst_n <= 1'b0;                      //初始复位
 	data_in <= 'd0;		
 	wr_en <= 1'b0;		
 	rd_en <= 1'b0;
@@ -49,7 +49,7 @@ module tb_sync_fifo_cnt();
 		@(negedge clk)begin		
 			rst_n <= 1'b1;				
 			wr_en <= 1'b1;		
-			data_in <= $random;			//生成8位随机数
+			data_in <= $random;	       //生成8位随机数
 		end
 	end
     //重复8次读操作，让FIFO读空	
